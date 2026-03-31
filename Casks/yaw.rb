91d35633935ce69afdd80391d9eacfc496187ae4
Jeff Yaw <jeff@yaw.sh>
2026-03-30 23:38:52 -0700

    Bump to v0.9.126

--- Casks/yaw.rb
+++ Casks/yaw.rb
@@ -1,11 +1,11 @@
 cask "yaw" do
   arch arm: "arm64", intel: "x64"
-  version "0.9.125"
+  version "0.9.126"
 
   if Hardware::CPU.arm?
-    sha256 "ba8a85d4f4e2b22d262595015949b9ba01e8c0cfdf4914add780b8c7dece8a29"
+    sha256 "fbf76bbd3696b93d6848cb775a5129569587dad1be4fd2f059e7c73b1ecfbab1"
   else
-    sha256 "f5dbd501c328371feb8dfb11bf51da4fadea4b524bf33138fe9cb71b781b107c"
+    sha256 "c9d5990c2e3f49415aad2489f180381e4d1f4f64b03b4db84e63c7824cc3361c"
   end
 
   url "https://yaw.sh/downloads/yaw-darwin-#{arch}-#{version}.zip"
